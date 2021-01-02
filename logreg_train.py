e405d73a65a92bedfb3edcd553b5878a1269a713
Dan <daniiomir@yandex.ru>
2021-01-02 21:42:09 +0300

    Started implementing model and metrics.

--- logreg_train.py
+++ logreg_train.py
@@ -1,6 +1,8 @@
 import src.tools as tools
+from src.model import LogisticRegression
 
 if __name__ == '__main__':
     args = tools.parse_args_train()
-    tools.read_dataset(args['dataset_path'])
+    dataset = tools.read_dataset(args['dataset_path'])
     # remove "Defense Against the Dark Arts" "Arithmancy" "Care of Magical Creatures"
+    model = LogisticRegression()
--- src/model.py
+++ src/model.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 class LogisticRegression:
-    pass
+    def __init__(self, eta):
+        self.eta = eta # learning rate
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
